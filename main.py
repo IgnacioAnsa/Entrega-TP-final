a22c39b1aa6dccc65ab0d46c3d78619f0a0ef737
Ignacio Sambuccetti <ignaciosambu@hotmail.com>
2024-12-05 15:39:11 -0300

    ultimas modificaciones

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from funciones.logica_juego import *
+from funciones.complementarias import *
 
 while True:
     
     print("\n--- Menú Principal ---")
     print("1 - Jugar Truco")
     print("2 - Salir")
+    print("3 - mostrar ranking historico")
+
     opcion = input("Elige una opción: ")
 
     match opcion:
@@ -14,6 +17,8 @@ while True:
         case "2":
             print("¡Gracias por jugar! Hasta la próxima.")
             break
+        case "3":
+            print("mostrar ranking historico")
+            print(leer_puntajes())
         case _:
             print("Opción inválida. Intenta nuevamente.")
-
